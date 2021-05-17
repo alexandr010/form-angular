372f506a0226c5fe2a4e8c99a33488db5470497e
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-05-17 13:06:23 +0300

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {formatDate} from "@angular/common";
 
 import { AppComponent } from './app.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
-// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormComponent
+    ReactiveFormComponent,
+
   ],
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        // NoopAnimationsModule
-    ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
